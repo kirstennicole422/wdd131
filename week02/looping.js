cb55e93c8f2bee86bbf77b18edd373b0142812c5
kirstennicole422 <kirstennicole422@gmail.com>
2025-05-14 23:30:34 -0600

    day of week loop

--- week02/looping.js
+++ week02/looping.js
@@ -1,15 +1,14 @@
-const DAYS = 6;
 const LIMIT = 30;
 let studentReport = [11, 42, 33, 64, 29, 37, 44]
 
-for (let i = 0; i < DAYS; i++) {
+for (let i = 0; i < studentReport.length; i++) {
     if (studentReport[i] < LIMIT) {
         console.log(studentReport[i])
     }
 }
 
 let i = 0
-while (i < DAYS) {
+while (i < studentReport.length) {
     if (studentReport[i] < LIMIT) {
         console.log(studentReport[i])
     }
@@ -29,21 +28,32 @@ for (let item in studentReport) {
 }
 
 
-let numberDays = 3;
-let dayCount = 0;
-let day = "";
+
+const DAYS = 6;
+let dayCount = 1;
+// let day = "";
 let dayNumber = 0;
 let today = new Date();
+let todayWord = new Intl.DateTimeFormat('en-US', {weekday: "long"}).format(today);
 
-console.log(`Today is ${today}`)
+console.log(`Today is ${todayWord}`);
 
-while (dayCount < numberDays) {
-    let day = new Date()
-    dayNumber = dayCount + today
-    day = //convert daynumber to day
-    console.log(`The next day is ${day}`)
-    dayCount++
+
+while (dayCount <= DAYS) {
+    let tomorrow = new Date();
+    tomorrow.setDate(today.getDate() + dayCount);
+    let tomorrowWord = new Intl.DateTimeFormat('en-US', {weekday: "long"}).format(tomorrow);
+    console.log(`The next day is ${tomorrowWord}`);
+    dayCount++;
 }
+
+// while (dayCount < DAYS) {
+//     let day = new Date()
+//     dayNumber = dayCount + today
+//     // day = //convert daynumber to day
+//     console.log(`The next day is ${new Intl.DateTimeFormat('en-US', {weekday: "long"}).format(today)}`)
+//     dayCount++
+// }
 
 
 // add one to day
