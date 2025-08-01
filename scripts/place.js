6ee47d5fe58000a19a364b3469dd2cb20af0b319
kirstennicole422 <kstockwell1@byupathway.edu>
2025-07-31 18:35:47 -0600

    added wind chill

--- scripts/place.js
+++ scripts/place.js
@@ -6,3 +6,13 @@ currentyear.innerHTML = `${today.getFullYear()} `;
 
 lastModified.innerHTML = `Last modification: ${document.lastModified}`;
 
+const windSpeed = document.querySelector(".wind-speed").innerHTML;
+const temp = document.querySelector(".temp").innerHTML;
+
+function calculateWindChill(windSpeed, temp) {
+    return Math.round(13.12 + (0.6215 * temp) - (11.37 * (windSpeed ** 0.16)) + (0.3965 * temp * (windSpeed ** 0.16)))
+}
+
+if (windSpeed > 4.8 && temp <= 10) {
+    document.querySelector("#wind-chill").innerHTML = `${calculateWindChill(windSpeed, temp)}° C`;
+} else { document.querySelector("#wind-chill").innerHTML = "N/A"; }
