be71bf39e983936d510b3c64de88c09f7a0a8ae8
kirstennicole422 <kstockwell1@byupathway.edu>
2026-01-07 03:41:24 -0700

    static now?

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,9 +1,14 @@
 const date = new Date();
 document.querySelector("#currentyear").innerHTML = date.getFullYear();
-document.querySelector("#lastModified").innerHTML = `Last Modified: ${String(date.getMonth() + 1).padStart(2, "0")}`
-    + `/${String(date.getDate()).padStart(2, "0")}`
-    + `/${date.getFullYear()} `
-    + `${String(date.getHours()).padStart(2, "0")}`
-    + `:${String(date.getMinutes()).padStart(2, "0")}:`
-+`${String(date.getSeconds()).padStart(2, "0")}`;
-    // + `:${toLocaleString(en-US, new Date()).getFullYear()}`;
+document.querySelector("#lastModified").innerHTML = `Last Modified: `
+    // + `${String(date.getMonth() + 1).padStart(2, "0")}`
+    // + `/${String(date.getDate()).padStart(2, "0")}`
+    // + `/${date.getFullYear()} `
+    // + `${String(date.getHours()).padStart(2, "0")}`
+    // + `:${String(date.getMinutes()).padStart(2, "0")}:`
+    // +`${String(date.getSeconds()).padStart(2, "0")}`;
+    // + `${date.toLocaleString('en-US', { dateStyle: "short" })}`
+    + `${date.toLocaleString('en-US', {date: "long" }, {timeStyle: "medium" })}`;    
+    
+    
+    // + `${date.toLocaleString('en-US', { year: "numeric" })}`;
