8a1147eef444b3426adfd16c50740479917b832b
kirstennicole422 <kstockwell1@byupathway.edu>
2026-01-07 23:17:29 -0700

    formatting changes

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,10 +1,4 @@
 const modificationdate = new Date(document.lastModified);
 
 document.querySelector("#currentyear").innerHTML = modificationdate.getFullYear();
-document.querySelector("#lastModified").innerHTML = `Last Modified: `
-    + `${String(modificationdate.getMonth() + 1).padStart(2, "0")}`
-    + `/${String(modificationdate.getDate()).padStart(2, "0")}`
-    + `/${modificationdate.getFullYear()}`
-    + ` ${String(modificationdate.getHours()).padStart(2, "0")}`
-    + `:${String(modificationdate.getMinutes()).padStart(2, "0")}:`
-    +`${String(modificationdate.getSeconds()).padStart(2, "0")}`;
+document.querySelector("#lastModified").innerHTML = `Last Modified: ${String(modificationdate.getMonth() + 1).padStart(2, "0")}/${String(modificationdate.getDate()).padStart(2, "0")}/${modificationdate.getFullYear()} ${String(modificationdate.getHours()).padStart(2, "0")}:${String(modificationdate.getMinutes()).padStart(2, "0")}:${String(modificationdate.getSeconds()).padStart(2, "0")}`;
