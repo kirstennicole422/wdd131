65f29e85d99596c2e321be087bea776e87467a34
kirstennicole422 <kstockwell1@byupathway.edu>
2026-01-07 23:11:38 -0700

    last modified progress

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,14 +1,19 @@
-const date = new Date();
-document.querySelector("#currentyear").innerHTML = date.getFullYear();
+const modificationdate = new Date(document.lastModified);
+// const today = new Date().
+// let dateandtime = today.toLocaleString('en-US', { date: "long" }, { timeStyle: "medium" });
+
+document.querySelector("#currentyear").innerHTML = modificationdate.getFullYear();
 document.querySelector("#lastModified").innerHTML = `Last Modified: `
-    // + `${String(date.getMonth() + 1).padStart(2, "0")}`
-    // + `/${String(date.getDate()).padStart(2, "0")}`
-    // + `/${date.getFullYear()} `
-    // + `${String(date.getHours()).padStart(2, "0")}`
-    // + `:${String(date.getMinutes()).padStart(2, "0")}:`
-    // +`${String(date.getSeconds()).padStart(2, "0")}`;
-    // + `${date.toLocaleString('en-US', { dateStyle: "short" })}`
-    + `${date.toLocaleString('en-US', {date: "long" }, {timeStyle: "medium" })}`;    
+    + `${String(modificationdate.getMonth() + 1).padStart(2, "0")}`
+    + `/${String(modificationdate.getDate()).padStart(2, "0")}`
+    + `/${modificationdate.getFullYear()}`
+    + `${String(modificationdate.getHours()).padStart(2, "0")}`
+    + `:${String(modificationdate.getMinutes()).padStart(2, "0")}:`
+    +`${String(modificationdate.getSeconds()).padStart(2, "0")}`;
     
+// + `${date.toLocaleString('en-US', { dateStyle: "short" })}`
+    // + `${dateandtime.toLocaleString('en-US', {date: "long" }, {timeStyle: "medium" })}`;    
+// + `${today.toLocaleString ('en-US', { date: "long"})}`;    
+// +`${modificationdate}`;
     
     // + `${date.toLocaleString('en-US', { year: "numeric" })}`;
