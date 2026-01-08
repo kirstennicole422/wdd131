33107ede38e3e69c9ae544d0b9fbed24458ca64e
kirstennicole422 <kstockwell1@byupathway.edu>
2026-01-07 23:14:28 -0700

    formatting adjustment

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,19 +1,10 @@
 const modificationdate = new Date(document.lastModified);
-// const today = new Date().
-// let dateandtime = today.toLocaleString('en-US', { date: "long" }, { timeStyle: "medium" });
 
 document.querySelector("#currentyear").innerHTML = modificationdate.getFullYear();
 document.querySelector("#lastModified").innerHTML = `Last Modified: `
     + `${String(modificationdate.getMonth() + 1).padStart(2, "0")}`
     + `/${String(modificationdate.getDate()).padStart(2, "0")}`
     + `/${modificationdate.getFullYear()}`
-    + `${String(modificationdate.getHours()).padStart(2, "0")}`
+    + ` ${String(modificationdate.getHours()).padStart(2, "0")}`
     + `:${String(modificationdate.getMinutes()).padStart(2, "0")}:`
     +`${String(modificationdate.getSeconds()).padStart(2, "0")}`;
-    
-// + `${date.toLocaleString('en-US', { dateStyle: "short" })}`
-    // + `${dateandtime.toLocaleString('en-US', {date: "long" }, {timeStyle: "medium" })}`;    
-// + `${today.toLocaleString ('en-US', { date: "long"})}`;    
-// +`${modificationdate}`;
-    
-    // + `${date.toLocaleString('en-US', { year: "numeric" })}`;
