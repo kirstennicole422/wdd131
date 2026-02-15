5a93d56e544da31813bc7f2b278d5722f4095522
kirstennicole422 <kstockwell1@byupathway.edu>
2026-02-14 17:13:37 -0700

    fix review text type

--- scripts/review.js
+++ scripts/review.js
@@ -2,7 +2,7 @@ let refresh = JSON.parse(localStorage.getItem("refreshCount")) || 0;
 refresh++;
 localStorage.setItem("refreshCount", JSON.stringify(refresh));
 if (refresh == 1) {
-    document.querySelector("#review-text").textContent = `This is your first review.0`;
+    document.querySelector("#review-text").textContent = `This is your first review.`;
 }
 else {
     document.querySelector("#review-text").textContent = `This is review number ${refresh}.`;
