f3d26f7b5f64570e38241745787cf83d39d2d98b
kirstennicole422 <kstockwell1@byupathway.edu>
2026-02-19 22:12:23 -0700

    hamburger bug adjustment

--- project/scripts/menu.js
+++ project/scripts/menu.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const navigation = document.querySelector(".nav-menu");
     const hamburgerButton = document.querySelector(".hamburger-button");
+    const mainHero = document.querySelector(".home");
     hamburgerButton.addEventListener("click", () => {
         navigation.classList.toggle("open-menu");
+        mainHero.classList.toggle("home-open-menu");
         if (navigation.classList.contains("open-menu")) {
             hamburgerButton.textContent = "✖";
         }
